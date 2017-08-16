ef70d4d27d605a1b68630c863eb2d8f62c221e66
Leonardo Marino-Ramirez <marino@marino-johnson.org>
2017-08-16 17:23:46 -0400

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -12,7 +12,7 @@ $rcmail_config['HOST'] = '';
 //	Duo Application Key. Generate yourself (at least 40 characters long) and keep it secret from Duo.
 //	You can generate a random string in Python with
 //	import os, hashlib
-//	print hashlib.sha1(os.urandom(32)).hexdigest()
+//	print hashlib.sha256(os.urandom(32)).hexdigest()
 $rcmail_config['AKEY'] = '';
 
 ?>
