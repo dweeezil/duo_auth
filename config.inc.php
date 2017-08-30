7bf8f76f34eaf756c9650283fc954168fd13f192
Leonardo Marino-Ramirez <marino@marino-johnson.org>
2017-08-30 12:02:48 -0400

    update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-//	Duo Integration Key. 
+//	Duo Integration Key
 $rcmail_config['IKEY'] = '';
 
 //	Duo Secret Key
@@ -10,12 +10,14 @@ $rcmail_config['SKEY'] = '';
 $rcmail_config['HOST'] = '';
 
 //	Duo trusted IPs
-$rcmail_config['2FA_OVERRIDE'] = array("127.0.0.1", "X.X.X.X", "Y.Y.Y.Y");
+//      Remove comment below to configure the array 
+//$rcmail_config['2FA_OVERRIDE'] = array("127.0.0.1", "X.X.X.X", "Y.Y.Y.Y");
 
 //	Duo Application Key. Generate yourself (at least 40 characters long) and keep it secret from Duo.
 //	You can generate a random string in Python with
 //	import os, hashlib
 //	print hashlib.sha256(os.urandom(32)).hexdigest()
-$rcmail_config['AKEY'] = '';
+//      *** Change the AKEY below ***
+$rcmail_config['AKEY'] = '86c57ce7cd8d7cd4ebe776f8cffe8cf7f5b1b2177cd772d02afc747ab9f457a2';
 
 ?>
