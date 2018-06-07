46942de087dff0a25d0e5e973f5820da523969cb
Marino-Ramirez <marino@marino-johnson.org>
2018-06-07 08:42:39 -0400

    update duo_auth.php

--- README.md
+++ README.md
@@ -9,13 +9,13 @@ It creates an additional page after successful username/password authentication 
 
 INSTALLATION
 ============
-
 Install using Composer (https://getcomposer.org) from the root directory of your roundcube installation:
 
-$ composer require lmr/duo_auth
+Run `$ composer require lmr/duo_auth`
 
 CONFIGURATION
 =============
+Copy `config.inc.php.dist` to `config.inc.php` and modify as necessary.
 Enter all keys necessary for integration with Duo in the config.inc.php file.
 Assuming a Duo integration has already been created in Duo's Admin Panel, you will be able to find all the information requested in the config.inc.php there.
 
@@ -24,3 +24,5 @@ CREDITS
 Author: Alexios Polychronopoulos - Wrote duo_auth for Roundcube.
 
 Author: Leonardo Mariño-Ramírez - Updated the plugin for compatibility with Roundcube 1.3.0.
+
+Author: LeJohnson Chow Added support for IPv4 CIDR matching and 2FA overrride for specific users.
--- duo_auth.php
+++ duo_auth.php
@@ -2,14 +2,20 @@
 /**
  * Two Factor Authentication using Duo Security for RoundCube
  *
- * @version 1.0.3
+ * @version 1.0.4
  *
  * Author(s): Alexios Polychronopoulos <dev@pushret.co.uk>
  * Author(s): Leonardo Marino-Ramirez <marino@marino-johnson.org>
- * Date: 08/16/2017
+ * Author(s): Johnson Chow <wschow@Comp.HKBU.Edu.HK>
+ * Date: 06/7/2018
  */
 
+
 require_once 'duo_web.php';
+require_once 'Client.php';
+require_once 'Auth.php';
+require_once 'Requester.php';
+require_once 'CurlRequester.php';
 
 class duo_auth extends rcube_plugin 
 {
@@ -31,12 +37,37 @@ class duo_auth extends rcube_plugin
 		
 		$this->register_handler('plugin.body', array($this, 'generate_html'));
 		
+		$ikey = $this->get('IKEY');
+		$skey = $this->get('SKEY');
+	        $host = $this->get('HOST');
+
+        	$user = trim(rcube_utils::get_input_value('_user', rcube_utils::INPUT_POST, true));
+		$D = new DuoAPI\Auth($ikey, $skey, $host);
+		$result=$D->preauth($user);
+
+		// bypass Roundcube users without DUO account
+		if($result["response"]["response"]["result"] == "auth") {
+		}
+		else {
+			$_SESSION['_Duo_2FAuth'] = True;
+			header('Location: ?_task=mail');
+		}
+
+		// bypass local users
+		if(in_array($user, $this->get('2FA_OVERRIDE_USERS'))) {
+                        $_SESSION['_Duo_2FAuth'] = True;
+                        header('Location: ?_task=mail');
+                }
+
+
 		// 2FA override with specific IPs 
-		if(in_array($_SERVER['REMOTE_ADDR'], $this->get('2FA_OVERRIDE'))) {
-						     $_SESSION['_Duo_2FAuth'] = True;
-						     header('Location: ?_task=mail');
-						     }
-		
+		foreach($this->get('2FA_OVERRIDE') as $ip) {
+			if($this->ipCIDRCheck($_SERVER['REMOTE_ADDR'],$ip)) {
+				$_SESSION['_Duo_2FAuth'] = True;
+				header('Location: ?_task=mail');
+			}
+		}
+
 		//indicates that user/pass authentication has succeeded.
 		$_SESSION['_Duo_Auth'] = True;
     	
@@ -140,4 +171,16 @@ class duo_auth extends rcube_plugin
 		exit;
 	}	
 	
+	private function ipCIDRCheck ($IP, $CIDR) {
+		if (!preg_match('/\//',$CIDR)) { $CIDR=$CIDR . "/32"; }
+
+		list ($net, $mask) = explode ('/', $CIDR);
+		$ip_net = ip2long ($net);
+		$ip_mask = ~((1 << (32 - $mask)) - 1);
+    		$ip_ip = ip2long ($IP);
+    		return (($ip_ip & $ip_mask) == ($ip_net & $ip_mask));
+	}
+
+
+
 }
