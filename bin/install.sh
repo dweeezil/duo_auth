4a013f755ab7c7ec98f6d14a3ebdab872f5e29ac
Leonardo Marino-Ramirez <marino@marino-johnson.org>
2023-06-10 22:06:03 -0400

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,5 @@
-<?php
-rename ("plugins/duo_auth/your_page_name_blocking_access_while_2FA_not_approved.htm", "../../your_page_name_blocking_access_while_2FA_not_approved.htm");
-rename ("plugins/duo_auth/your_page_name_redirect.php", "../../your_page_name_redirect.php");
-rename ("plugins/duo_auth/duo.png", "../../duo.png");
-?>
+#!/bin/bash
+
+mv plugins/duo_auth/your_page_name_blocking_access_while_2FA_not_approved.htm ../../your_page_name_blocking_access_while_2FA_not_approved.htm
+mv plugins/duo_auth/your_page_name_redirect.php ../../your_page_name_redirect.php
+mv plugins/duo_auth/duo.png ../../duo.png
