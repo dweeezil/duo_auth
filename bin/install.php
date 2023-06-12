8bb15b22eec962440286cea3da227291fa20a3dd
Leonardo Marino-Ramirez <marino@marino-johnson.org>
2023-06-12 09:22:47 -0400

    Script updates.

--- bin/install.php
+++ bin/install.php
@@ -1,5 +1,5 @@
 <?php
-rename ("plugins/duo_auth/your_page_name_blocking_access_while_2FA_not_approved.htm", "your_page_name_blocking_access_while_2FA_not_approved.htm");
-rename ("plugins/duo_auth/your_page_name_redirect.php", "your_page_name_redirect.php");
-rename ("plugins/duo_auth/duo.png", "duo.png");
+rename ("./plugins/duo_auth/your_page_name_blocking_access_while_2FA_not_approved.htm", "./your_page_name_blocking_access_while_2FA_not_approved.htm");
+rename ("./plugins/duo_auth/your_page_name_redirect.php", "./your_page_name_redirect.php");
+rename ("./plugins/duo_auth/duo.png", "./duo.png");
 ?>
