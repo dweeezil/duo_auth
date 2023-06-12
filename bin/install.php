98b72c7f6e8af03d2e9bdd4194f7a20c95edf48a
Leonardo Marino-Ramirez <marino@marino-johnson.org>
2023-06-12 09:59:20 -0400

    Update install.php
    Path updates

--- bin/install.php
+++ bin/install.php
@@ -1,5 +1,5 @@
 <?php
-rename ("./duo_auth/your_page_name_blocking_access_while_2FA_not_approved.htm", "../../your_page_name_blocking_access_while_2FA_not_approved.htm");
-rename ("./duo_auth/your_page_name_redirect.php", "../../your_page_name_redirect.php");
-rename ("./duo_auth/duo.png", "../../duo.png");
+rename ("./duo_auth/your_page_name_blocking_access_while_2FA_not_approved.htm", "../your_page_name_blocking_access_while_2FA_not_approved.htm");
+rename ("./duo_auth/your_page_name_redirect.php", "../your_page_name_redirect.php");
+rename ("./duo_auth/duo.png", "../duo.png");
 ?>
